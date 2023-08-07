edf2bd8e95e288ed22b648456e2c6c739a5ee0a9
paulaan <me@paulaan.me>
2023-08-07 16:49:04 +0700

    Dummy demo from signal data

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ from fastapi import FastAPI, WebSocket, Request
 from model import IoTModel, load_dataframe
 import argparse
 from config import FAILURE_PROMPT, ANOMALY_PROMPT
-import pandas as pd
 from iot import SensorData, SensorDataDetection, IoTDataBuffer, mock_mqtt
 import asyncio
 from fastapi.responses import HTMLResponse
-import datetime
-import websockets
 
-PORT = 8082
+PORT = 8083
 app = FastAPI(version="0.1")
 trainer = IoTModel()
 
@@ -17,59 +14,12 @@ iot_data = None  # mock_iot_mqtt data source
 data_buffer = IoTDataBuffer()
 anomaly_buffer = IoTDataBuffer()
 
-html = """
-<!-- index.html -->
-
-<html>
-<head>
-  <title>Sensor Data Viewer</title>
-</head>
-<body>
-
-  <h1>Real-time Sensor Data</h1>
-
-  <table>
-    <tr>
-      <th>Timestamp</th>
-      <th>Vibration</th>
-      <th>Rotation</th>  
-      <th>Pressure</th>
-      <th>Voltage</th>
-    </tr> 
-    <tbody id="data"></tbody>
-  </table>
-
-  <script>
-    const socket = new WebSocket("ws://localhost:PORT/ws/sensor");
-    try {
-    
-    socket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      if (data.volt > 0){
-      const row = `
-        <tr>
-          <td>${data.timestamp}</td>
-          <td>${data.vibration}</td>  
-          <td>${data.rotate}</td>
-          <td>${data.pressure}</td>
-          <td>${data.volt}</td>    
-        </tr>`;
-      document.getElementById('data').innerHTML += row;
-      }
-    }
-    } catch (exceptionVar) {
-        console.log(exceptionVar)
-    } finally {
-    }
-  </script>
-
-</body>
-</html>
-""".replace("PORT", "{0}".format(PORT))
-
 
 @app.get("/index", response_class=HTMLResponse)
 async def get_index(request: Request):
+    with open("index.html", "r") as f:
+        html = f.read()
+    html = html.replace("PORT", "{0}".format(PORT))
     return HTMLResponse(content=html)
 
 
@@ -79,6 +29,7 @@ async def sensor_endpoint(*, websocket: WebSocket):
     while True:
         new_data = await data_buffer.get_next_data()
         if new_data is not None:
+            print(f"Received and Push to buffer: {new_data}")
             await anomaly_buffer.add_data(new_data)
             await websocket.send_json(new_data.dict())
         await websocket.send_json({})
@@ -91,7 +42,7 @@ async def websocket_mqtt_endpoint(*, websocket: WebSocket):
         data = await websocket.receive_text()
         sensor_data = SensorData.parse_raw(data)
 
-        print(f"Received and Push to buffer: {data}")
+        print(f"PUSSHING and Push to buffer: {data}")
         await data_buffer.add_data(sensor_data)
         await websocket.send_json({})
 
@@ -119,6 +70,13 @@ def get_app_version():
     return {"version": app.version}
 
 
+@app.get("/fake_data")
+def get_app_version():
+    iot_data = load_dataframe(args.train_files, args.metadata_file)
+    asyncio.run(mock_mqtt(iot_data, PORT))
+    return {"version": app.version}
+
+
 @app.post("/failure")
 async def model_failure_detection(prompt: SensorData):
     return trainer.generate(FAILURE_PROMPT.format(prompt.volt, prompt.vibration, prompt.pressure, prompt.rotate))
@@ -140,26 +98,20 @@ if __name__ == "__main__":
     parser.add_argument('--mqtt', type=bool, help='Enable Mqtt')
 
     args = parser.parse_args()
-
-    if args.mqtt:
-        iot_data = load_dataframe(args.train_files, args.metadata_file)
-        asyncio.run(mock_mqtt(iot_data, PORT))
+    trainer.load_model(model_name=args.train_base_model)  # google/flan-t5-base
+    if args.train:
+        print(f'Training files: {args.train_files}')
+        print(f'Metadata file: {args.metadata_file}')
+        print(f'Start training model {args.train_base_model} with adapter name {args.trained_new_name}')
+        trainer.train(args.train_files.split(','),
+                      args.metadata_file,
+                      new_peft_model_name=args.trained_new_name)
         pass
     else:
-        trainer.load_model(model_name=args.train_base_model)  # google/flan-t5-base
-        if args.train:
-            print(f'Training files: {args.train_files}')
-            print(f'Metadata file: {args.metadata_file}')
-            print(f'Start training model {args.train_base_model} with adapter name {args.trained_new_name}')
-            trainer.train(args.train_files.split(','),
-                          args.metadata_file,
-                          new_peft_model_name=args.trained_new_name)
-            pass
-        else:
-            lora_model = '{0}_{1}'.format(args.train_base_model, args.trained_new_name).replace('/', '_')
-            print(f'Loading existing model: ', lora_model)
-            trainer.load_lora('lora/{0}'.format(lora_model))
-            print(f'Loaded existing model success')
-        import uvicorn
+        lora_model = '{0}_{1}'.format(args.train_base_model, args.trained_new_name).replace('/', '_')
+        print(f'Loading existing model: ', lora_model)
+        trainer.load_lora('lora/{0}'.format(lora_model))
+        print(f'Loaded existing model success')
+    import uvicorn
 
-        uvicorn.run(app, host='0.0.0.0', port=PORT)
+    uvicorn.run(app, host='0.0.0.0', port=PORT)
